2d217dfeaae938eafc3100785e05f52a390b5f94
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 16:58:54 +0530

    Update first5mul.py

--- first5mul.py
+++ first5mul.py
@@ -1,10 +1,12 @@
-lst=[]
 class multiply:
   def fivemul(self,n):
     for i in range(1,6):
       lst.append(i*n)
     for i in range(0,len(lst)):
-    	print(lst[i],end=" ")
+    	if lst[i]==lst[len(lst)-1]:
+    		print(lst[i])
+    	else:
+    		print(lst[i],end=" ")
 n=int(input())
 call=multiply()
 call.fivemul(n)
