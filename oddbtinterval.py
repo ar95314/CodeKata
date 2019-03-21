2c4c5dc026e89aba25637a92bee9f5309c535a13
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 14:59:38 +0530

    Create oddbtinterval.py
    To find odd numbers between given intervals

---
+++ oddbtinterval.py
@@ -0,0 +1,8 @@
+class calculate:
+  def oddinterval(self,start,end):
+    for i in range(start+1,end):
+      if(i%2!=0):
+        print(i,"",end="")
+start,end=map(int,input().split())
+cal=calculate()
+cal.oddinterval(start,end)
