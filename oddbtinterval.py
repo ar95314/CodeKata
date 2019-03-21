7876c113fdab03b6f32172f4fdb7d1b7bdb9cba1
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 17:03:24 +0530

    Update oddbtinterval.py
    printing output in single line to avoid space @end in online compilers

--- oddbtinterval.py
+++ oddbtinterval.py
@@ -2,7 +2,10 @@ class calculate:
   def oddinterval(self,start,end):
     for i in range(start+1,end):
       if(i%2!=0):
-        print(i,"",end=" ")
+        if i==end-1:
+          print(i)
+        else:
+          print(i,end=" ")
 start,end=map(int,input().split())
 cal=calculate()
 cal.oddinterval(start,end)
