b6830b4acb595f5ca8ec4830830d1665c6424c51
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 16:33:48 +0530

    Update first5mul.py

--- first5mul.py
+++ first5mul.py
@@ -1,8 +1,9 @@
+lst=[]
 class multiply:
   def fivemul(self,n):
     for i in range(1,6):
-      print(i*n,end=" ")
+      lst=i*n
+      print(lst,end=" ")
 n=int(input())
 call=multiply()
 call.fivemul(n)
-    
