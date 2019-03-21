4919de0d1f75899c18d9daa21b260ed84e55f1de
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 17:18:44 +0530

    Update evenbtinverval.py

--- evenbtinverval.py
+++ evenbtinverval.py
@@ -1,11 +1,12 @@
 class calculate:
-  def oddinterval(self,start,end):
+  def eveinterval(self,start,end):
     for i in range(start+1,end):
       if(i%2==0):
-        if i==end-1:
+        if i==(end-1):
+          print(end)
           print(i)
         else:
           print(i,end=" ")
 start,end=map(int,input().split())
 cal=calculate()
-cal.oddinterval(start,end)
+cal.eveinterval(start,end)
