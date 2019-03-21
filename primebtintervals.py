48095b17e86c08406a1452d24f764f1b726c080c
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 17:45:50 +0530

    Update primebtintervals.py

--- primebtintervals.py
+++ primebtintervals.py
@@ -1,14 +1,18 @@
+lst=[]
 class calprime:
 	def prime(self,start,end):
 		for num in range(start+1,end):
 			for i in range(2,num):
 				if num%i==0:
 					break
+				
 			else:
-				if num==(end-1):
-					print(num)
-				else:
-					print(num,end=" ")
+				lst.append(num)
+		for i in range(0,len(lst)):
+			if i == (len(lst)-1):
+				print(lst[i])
+			else:
+				print(lst[i],end=" ")
 start,end=map(int,input().split())
 c=calprime()
 c.prime(start,end)
