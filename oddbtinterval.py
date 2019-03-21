92146ff7e026ab1f7cb7fd70d9213a2a07889adc
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-21 15:02:21 +0530

    Update oddbtinterval.py

--- oddbtinterval.py
+++ oddbtinterval.py
@@ -2,7 +2,7 @@ class calculate:
   def oddinterval(self,start,end):
     for i in range(start+1,end):
       if(i%2!=0):
-        print(i,"",end="")
+        print(i,"",end=" ")
 start,end=map(int,input().split())
 cal=calculate()
 cal.oddinterval(start,end)
