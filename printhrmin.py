d309678122de13b0ce13fd6f7083554d75502401
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-22 21:21:07 +0530

    Update printhrmin.py

--- printhrmin.py
+++ printhrmin.py
@@ -1,11 +1,11 @@
 class time:
 	def printhrmin(self,min):
 		if min<60:
-			print(0,"",min)
+			print(0,min)
 		else:
 			dig=int(min/60)
 			minit=min-(dig*60)
-			print(dig,"",minit)
+			print(dig,minit)
 min=int(input())
 call=time()
 call.printhrmin(min)
