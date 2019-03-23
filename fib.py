5edb3578464bdc7ab21a866a8dcc551920785b7a
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-23 12:10:24 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,21 +1,24 @@
 lst=[]
 class fib:
 	def create(self,n):
-		fib=0
-		a=b=1
-		lst.append(a)
-		lst.append(b)
-		while(n-2):
-			fib=a+b
-			a=b
-			b=fib
-			lst.append(fib)
-			n=n-1
-		for i in range(len(lst)):
-			if i==(len(lst)-1):
-				print(lst[i])
-			else:
-				print(lst[i],end=" ")
+		if n>1:
+			fib=0
+			a=b=1
+			lst.append(a)
+			lst.append(b)
+			while(n-2):
+				fib=a+b
+				a=b
+				b=fib
+				lst.append(fib)
+				n=n-1
+			for i in range(0,len(lst)):
+				if i==(len(lst)-1):
+					print(lst[i])
+				else:
+					print(lst[i],end=" ")
+		else:
+			print("1")
 n=int(input())
 call=fib()
 call.create(n)
