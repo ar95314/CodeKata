821af24d4bce0ece0f04590290d2ddb66e4e5a4e
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-25 21:03:56 +0530

    Update checkifperfectsq.py

--- checkifperfectsq.py
+++ checkifperfectsq.py
@@ -1,10 +1,13 @@
 class check:
 	def ifperfect(self,n,m):
 		prod=n*m
-		if n==m:
+		if prod==0:
 			print("yes")
-		else:
-			print("no")
+		elif prod!=0:
+			if n==m:
+				print("yes")
+			else:
+				print("no")
 n,m=map(int,input().split())
 call=check()
 call.ifperfect(n,m)
