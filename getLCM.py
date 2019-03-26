b1d98048eeb03e87430fdb580a759ca1bccc8eda
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-03-26 21:45:09 +0530

    Create getLCM.py

---
+++ getLCM.py
@@ -0,0 +1,15 @@
+class lcm:
+	def checkit(self,n,m):
+		if n > m:
+			greater = n
+		else:
+			greater = m
+		while(True):
+			if((greater %n  == 0) and (greater % m == 0)):
+				lcm = greater
+				break
+				greater += 1
+		print(lcm)
+n,m=map(int,input().split())
+call=lcm()
+call.checkit(n,m)
