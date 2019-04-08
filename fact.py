5738463468293f1ca1191ff88c27ddae2e8a3347
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-04-08 20:41:49 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,9 +1,9 @@
-class factorial:
-  def fact(self,num):
+class fact:
+  def factorial(self,num):
     f=1
     for i in range(1,num+1):
       f=f*i
     print(f)
 num=int(input())
-call=factorial()
-call.fact(num)
+call=fact()
+call.factorial(num)
