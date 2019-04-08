0c5ab68628160cb3a685bd89a006c6b97e78bb5b
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-04-08 21:04:43 +0530

    Create append'.'@end.py

---
+++ append'.'@end.py
@@ -0,0 +1,14 @@
+string=input()
+lst=[]
+n=len(string)
+for i in string:
+	lst.append(i)
+for i in range(len(lst)):
+	if i==len(lst)-1:
+		lst.append('.')
+print(lst)
+for i in range(len(lst)):
+	if i == len(lst)-1:
+		print(lst[i])
+	else:
+		print(lst[i],end="")
