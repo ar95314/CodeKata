e5f0e6e3007250792cbe063a9767af42a64e4168
Dhanusha R <48701982+DhanushaRamesh@users.noreply.github.com>
2019-04-09 21:18:19 +0530

    Update findrepeatinlst.py

--- findrepeatinlst.py
+++ findrepeatinlst.py
@@ -7,11 +7,14 @@ class findrepeat:
 				if lst[i]==lst[i+1]:
 					if lst[i] not in lst1:
 						lst1.append(lst[i])
-		for i in range(len(lst1)):
-			if i==len(lst1)-1:
-				print(lst1[i])
-			else:
-				print(lst1[i],end=" ")
+		if len(lst1)==0:
+			print("unique")
+		else:
+			for i in range(len(lst1)):
+				if i==len(lst1)-1:
+					print(lst1[i])
+				else:
+					print(lst1[i],end=" ")
 n=int(input())
 lst=list(map(int,input().split()))
 call=findrepeat()
