c92303cb7cd9162bab73d949d93f12799a504174
Amit Rathore <36275327+ar95314@users.noreply.github.com>
2019-04-11 19:15:33 +0530

    Update checkifcomposite.py

--- checkifcomposite.py
+++ checkifcomposite.py
@@ -1,11 +1,11 @@
 class checkcomposite:
-  def notprime(self,n):
-    for i in range(2,n):
-      if n%i==0:
+  def notprime(self,m):
+    for i in range(2,m):
+      if m%i==0:
         print("yes")
         break
     else:
       print("no")
-n=int(input())
+m=int(input())
 c=checkcomposite()
 c.notprime(n)
